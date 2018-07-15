adf48a84a5141e3aaab3145b32dc3c46b7ffdca2
nivedhamadhavan <40656001+nivedhamadhavan@users.noreply.github.com>
2018-07-15 20:00:39 +0530

    Add files via upload

---
+++ getinpt.py
@@ -0,0 +1,6 @@
+a=input ("enter name:")
+print(a)
+b=int(input("enter age:"))
+print(b)
+c=int(input("enter regno:"))
+print(c)
---
+++ greatest.py
@@ -0,0 +1,9 @@
+a=20
+b=30
+c=25
+if(a>b)&(a>c):
+    print("a is greatest",a)
+elif(b>c):
+    print("b is greatest",b)
+else:
+    print("c is greatest",c)
